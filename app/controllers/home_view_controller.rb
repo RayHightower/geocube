87a27e21ba62ea730a55a1209edb8976179ef17a
Michael Erasmus <michaelerasmus@gmail.com>
2012-09-23 00:58:15 +0200

    Before adding views

---
+++ app/controllers/home_view_controller.rb
@@ -0,0 +1,16 @@
+class HomeViewController < UIViewController
+  def viewDidLoad
+    super
+
+    # Do any additional setup after loading the view.
+  end
+
+  def viewDidUnload
+    super
+    # Release any retained subviews of the main view.
+  end
+
+  def shouldAutorotateToInterfaceOrientation(interfaceOrientation)
+    interfaceOrientation == UIInterfaceOrientationPortrait
+  end
+end
