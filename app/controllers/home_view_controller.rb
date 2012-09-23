e5e27f8153e29d2e76741b3283da078da9ce2b1f
Michael Erasmus <michaelerasmus@gmail.com>
2012-09-23 10:34:14 +0200

    Before REPL work

--- app/controllers/home_view_controller.rb
+++ app/controllers/home_view_controller.rb
@@ -1,8 +1,30 @@
 class HomeViewController < UIViewController
   def viewDidLoad
     super
+    scroll_frame = view.bounds
 
-    # Do any additional setup after loading the view.
+    scroll = UIScrollView.alloc.initWithFrame(scroll_frame)
+    scroll.pagingEnabled = true
+    scroll.contentSize = CGSizeMake(scroll_frame.width * 3, scroll_frame.height)
+
+    3.times do |i|
+      subframe = scroll_frame.right(scroll_frame.width * i)
+      subview = UIView.alloc.initWithFrame(subframe)
+      subview.backgroundColor = [rand * 255, rand * 255, rand * 255].uicolor
+      scroll << subview
+      header_frame = subview.bounds.down(30).right(30).width(150).height(20)
+      header_label = UILabel.alloc.initWithFrame header_frame
+      header_label.text = "This is page #{i + 1}"
+      header_label.backgroundColor = :clear.uicolor
+      subview << header_label
+
+      sublabel = UILabel.alloc.initWithFrame(header_frame.below(100).centered_in(subframe))
+      sublabel.text = "lorem ipsum dolor amet..."
+      sublabel.backgroundColor = :clear.uicolor
+      subview << sublabel
+    end
+
+    view << scroll
   end
 
   def viewDidUnload
